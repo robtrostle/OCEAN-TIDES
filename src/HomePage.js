4aad993bbfda65f336672e7537662da4c0fd6ee8
Rob Trostle <rtrostle@nipr.com>
2024-08-01 08:48:26 -0400

    fix date single digits

--- src/HomePage.js
+++ src/HomePage.js
@@ -15,12 +15,12 @@ const HomePage = () => {
     return `${year}${month}${day}`;
   };
 
-  // Get today's date in the format YYYYMMDD
+  // Get today's date in the format mmddyyyy
   const getToday = () => {
     const date = new Date();
     const year = date.getFullYear();
-    const month = String(date.getMonth() + 1).padStart(2, '0');
-    const day = String(date.getDate()).padStart(2, '0');
+    const month = String(date.getMonth() + 1);
+    const day = String(date.getDate());
     return `${month}/${day}/${year}`;
   };
 
