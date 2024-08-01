c39bdf6f808b71291373e2dc6a75ea535350214c
Rob Trostle <rtrostle@nipr.com>
2024-08-01 11:24:23 -0400

    fixing url again back to dates

--- src/HomePage.js
+++ src/HomePage.js
@@ -31,7 +31,7 @@ const HomePage = () => {
       const today = getFormattedDate();
       try {
         const response = await fetch(
-          `https://api.tidesandcurrents.noaa.gov/api/prod/datagetter?station=8720259&product=predictions&datum=MLLW&time_zone=lst_ldt&interval=hilo&units=english&date=${today}&format=json`
+          `https://api.tidesandcurrents.noaa.gov/api/prod/datagetter?begin_date=${today}&end_date=${today}&station=8720259&product=predictions&datum=MLLW&time_zone=lst_ldt&interval=hilo&units=english&format=json`
         );
         if (!response.ok) {
           throw new Error('Network response was not ok');
