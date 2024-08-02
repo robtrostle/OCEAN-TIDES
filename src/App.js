3cf0a305ff30cb127c04243d0a2750d2374b5fac
Rob Trostle <rtrostle@nipr.com>
2024-08-02 14:51:57 -0400

    adding see-tomorrow feature

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import SeeTomorrow from './components/SeeTomorrow';
 import HomePage from './HomePage';
 
 
 
 function App() {
   return (
-    <div>
-      <HomePage />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/see-tomorrow" element={<SeeTomorrow />} />
+      </Routes>
+    </Router>
   );
 }
 
 export default App;
-
